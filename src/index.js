5165ddd1bcc2618ccd490b9727c51c90f7211faa
Nishant Raghav <nishant@nexgsolution.com>
2020-07-30 23:27:57 +0530

    adding test commit

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+
+//adding test comment
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
