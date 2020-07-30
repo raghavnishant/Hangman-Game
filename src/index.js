59bb738a04b051d900bc1b3af6f0301dbfab478c
Nishant Raghav <nishant.raghav006@outlook.com>
2020-07-30 23:51:09 +0530

    removing test commit

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-
-//adding test comment
+ 
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
